f22c06ad2ae41cf59694483f00c44927c159d792
Irene Chang <irene84111@gmail.com>
2016-04-28 12:26:09 +0800

    speaker modified

--- source/js/main.js
+++ source/js/main.js
@@ -1,9 +1,3 @@
-/*
-	Photon by HTML5 UP
-	html5up.net | @n33co
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
-
 (function($) {
 
     skel.breakpoints({
@@ -58,6 +52,29 @@
       });
       $('img.lazy').lazyload({
         effect : 'fadeIn'
+      });
+      
+      // Slider / owl-carousel
+      $(document).ready(function(){
+        $('.owl-carousel').owlCarousel({
+          responsive:{
+            0:{items:1},
+            600:{items:2},
+            768:{items:3},
+            992:{items:4}
+          },
+          loop:true,
+          margin:30,
+          dots: false,
+          nav:false,
+          navText: ['<i class="fa fa-caret-left" aria-hidden="true"></i>','<i class="fa fa-caret-right" aria-hidden="true"></i>'],
+          autoplay: true,
+          autoplayTimeout: 2500,
+          autoplayHoverPause: true,
+          center : false,
+          lazyLoad: true
+          
+        })
       });
 
     });
