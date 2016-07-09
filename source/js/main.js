581a929a165a3919c46854602726e874f9fb187b
WebberWu <bird23074035@gmail.com>
2016-07-09 10:48:10 +0800

    圖片路徑改成絕對路徑

--- source/js/main.js
+++ source/js/main.js
@@ -46,11 +46,11 @@
 
     // backstretch
     $('#header').backstretch([
-      "./dist/images/jsdc-scenario-1.jpg",
-      "./dist/images/jsdc-scenario-2.jpg",
-      "./dist/images/jsdc-scenario-3.jpg",
-      "./dist/images/jsdc-scenario-4.jpg",
-      "./dist/images/jsdc-scenario-5.jpg",
+      "/dist/images/jsdc-scenario-1.jpg",
+      "/dist/images/jsdc-scenario-2.jpg",
+      "/dist/images/jsdc-scenario-3.jpg",
+      "/dist/images/jsdc-scenario-4.jpg",
+      "/dist/images/jsdc-scenario-5.jpg",
     ], {duration: 3000, fade: 1200, lazyload: true,});
 
     // lazyload
